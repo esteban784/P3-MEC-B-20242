75c00cbd67a02aa11ab5ff301a7d968196c6659d
esteban784 <est.william.erodri2@unimilitar.edu.co>
2024-08-27 14:32:04 -0500

    Update pg1.java

--- P1/pg1.java
+++ P1/pg1.java
@@ -1,1 +1,25 @@
+##crear una calse llamda fraccion con atributos correspondientes a numerador y denominador aplicar encapsulamiento y almenos un metodo set y get 
+para manipular objetos de esa clase.
 
+class Fraccion:
+    def __init__(self, numerador, denominador):
+        self.__numerador = numerador
+        self.__denominador = denominador
+    def get_numerador(self):
+        return self.__numerador
+
+    def get_denominador(self):
+        return self.__denominador
+
+ 
+    def set_numerador(self, numerador):
+        self.__numerador = numerador
+
+    def set_denominador(self, denominador):
+        if denominador != 0:  # Para evitar división por cero
+            self.__denominador = denominador
+        else:
+            raise ValueError("El denominador no puede ser cero.")
+
+    def __str__(self):
+        return f"{self.__numerador}/{self.__denominador}"
